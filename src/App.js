5ac57efb56315707e5caf11740832fa08088f86d
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2021-12-09 12:06:35 -0600

    CartContext and Shopping cart taken care of

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Contexts
 import ProductContext from './contexts/ProductContext';
-
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -22,6 +22,7 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -34,6 +35,7 @@ function App() {
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
